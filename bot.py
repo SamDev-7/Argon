d685e0ff5991a05126aeee5fbc07b0fc512875a9
SamDev-7 <hello@saml.is-a.dev>
2022-08-09 19:50:05 -0400

    add startup message

--- bot.py
+++ bot.py
@@ -1,8 +1,10 @@
 import os
+import aiohttp
 import disnake
+import asyncio
 from disnake.ext import commands
 
-bot = commands.Bot(command_prefix=disnake.ext.commands.when_mentioned, owner_id=790675967982436363, activity=disnake.Game(name='Mention me to chat!'))
+bot = commands.Bot(command_prefix=commands.when_mentioned, owner_id=790675967982436363, activity=disnake.Game(name='Starting...'), status=disnake.Status.dnd)
 bot.remove_command('help')
 
 cogs = ['info', 'listen_msgs']
@@ -11,6 +13,24 @@ cogs = ['info', 'listen_msgs']
 async def on_ready():
     
     bot.load_extension("jishaku")
+
+    print("Waiting for RASA servers...")
+
+    async with aiohttp.ClientSession(timeout=aiohttp.ClientTimeout(total=5)) as session:
+        while True:
+            try:
+                async with session.get('http://localhost:5005/') as resp:
+                    if resp.status == 200:
+                        async with session.get(f'http://localhost:5055?token={os.getenv("RASA_TOKEN")}') as resp2:
+                            if resp2.status == 404:
+                                await bot.change_presence(activity=disnake.Game(name='Mention me to chat!'), status=disnake.Status.online)
+                                break
+                            else:
+                                raise Exception("Action server not ready")
+                    else:
+                        raise Exception("Rasa server not ready")
+            except:
+                await asyncio.sleep(5)
 
     for cog in cogs:
         bot.load_extension(f"cogs.{cog}")
